5b3328de5477707822fbf82a0994668f6f7fbe6e
Greg Schullo <greg.schullo@gmail.com>
2019-11-29 15:30:08 -0700

    added new exercise files

---
+++ favorite-number.py
@@ -0,0 +1,7 @@
+# Author: Greg Schullo
+# Python Crash Course, 2nd Edition
+# Excercise: 2-9
+
+FAVORITE_NUMBER = 9
+message = "My favorite number is " + str(FAVORITE_NUMBER)
+print(message)
---
+++ number-eight.py
@@ -0,0 +1,8 @@
+# Author: Greg Schullo
+# Python Crash Course, 2nd Edition
+# Excercise: 2-8
+
+print(5+3)
+print(9-1)
+print(2*4)
+print(16/2)
