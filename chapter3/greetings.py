f75e637a7e88522ad888f2f9623fb0315df868ff
Greg Schullo <greg.schullo@gmail.com>
2020-03-23 19:03:17 -0600

    updated file structure

---
+++ chapter3/greetings.py
@@ -0,0 +1,11 @@
+names = ['Greg', 'Emily', 'Ryan', 'Alex', 'Garrett']
+message0 = "Hello " + names[0] + ", how are you doing?"
+message1 = "Hello " + names[1] + ", how are you doing?"
+message2 = "Hello " + names[2] + ", how are you doing?"
+message3 = "Hello " + names[3] + ", how are you doing?"
+message4 = "Hello " + names[4] + ", how are you doing?"
+print(message0)
+print(message1)
+print(message2)
+print(message3)
+print(message4)
---
+++ chapter3/names.py
@@ -0,0 +1,6 @@
+names = ['Greg', 'Emily', 'Ryan', 'Alex', 'Garrett']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
